56dd61190c9ed402829691d4410d373fe70b9c39
Florin Balint <balint.florin@gmail.com>
2023-10-25 12:54:39 +0200

    results

--- src/test/java/com/example/demo3/CalculatorAPITest.java
+++ src/test/java/com/example/demo3/CalculatorAPITest.java
@@ -11,6 +11,7 @@ import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,4 +38,58 @@ class CalculatorAPITest {
 				.consumeWith(result -> assertEquals(new BigDecimal("5"), result.getResponseBody().getResult()));
 	}
 
+	@Test
+	@DisplayName("Testing calculator operation: multiplication \uD83D\uDE80")
+	public void multiplicationTest() {
+		CalculationRequest request = new CalculationRequest();
+		request.setNumber1(new BigDecimal("2"));
+		request.setNumber2(new BigDecimal("3"));
+		request.setOperation("*");
+
+		this.webTestClient
+				.post()
+				.uri("/api/calculator")
+				.body(Mono.just(request), CalculationRequest.class)
+				.exchange()
+				.expectStatus().is2xxSuccessful() // check status code
+				.expectBody(CalculationResponse.class)
+				.consumeWith(result -> assertEquals(new BigDecimal("6"), result.getResponseBody().getResult()));
+	}
+
+	@Test
+	@DisplayName("Testing calculator operation: division \uD83D\uDE80")
+	public void divisionTest() {
+		CalculationRequest request = new CalculationRequest();
+		request.setNumber1(new BigDecimal("15"));
+		request.setNumber2(new BigDecimal("3"));
+		request.setOperation("/");
+
+		this.webTestClient
+				.post()
+				.uri("/api/calculator")
+				.body(Mono.just(request), CalculationRequest.class)
+				.exchange()
+				.expectStatus().is2xxSuccessful() // check status code
+				.expectBody(CalculationResponse.class)
+				.consumeWith(result -> assertTrue(new BigDecimal("5")
+						.compareTo(result.getResponseBody().getResult()) == 0));
+	}
+
+	@Test
+	@DisplayName("Testing calculator operation: subtraction \uD83D\uDE80")
+	public void subtractionTest() {
+		CalculationRequest request = new CalculationRequest();
+		request.setNumber1(new BigDecimal("5"));
+		request.setNumber2(new BigDecimal("3"));
+		request.setOperation("-");
+
+		this.webTestClient
+				.post()
+				.uri("/api/calculator")
+				.body(Mono.just(request), CalculationRequest.class)
+				.exchange()
+				.expectStatus().is2xxSuccessful() // check status code
+				.expectBody(CalculationResponse.class)
+				.consumeWith(result -> assertEquals(new BigDecimal("2"), result.getResponseBody().getResult()));
+	}
 }
--- src/test/java/com/example/demo3/CarOrderTest.java
+++ src/test/java/com/example/demo3/CarOrderTest.java
@@ -2,15 +2,18 @@ package com.example.demo3;
 
 import com.example.demo3.car.Car;
 import com.example.demo3.car.CarOrder;
-import com.example.demo3.model.model.CalculationRequest;
-import com.example.demo3.model.model.CalculationResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CarOrderTest {
@@ -22,6 +25,29 @@ public class CarOrderTest {
     @DisplayName("Autobestellung von Fritz")
     public void carOrderTest() {
         // Bestelle mindestens 100 Autos in 3 unterschiedliche Farben
+        for (int i = 0; i <= 100; i++) {
+            CarOrder request = new CarOrder();
+            if (i < 30) {
+                request.setColor("red");
+            } else if (i >= 30 && i < 60) {
+                request.setColor("white");
+            } else {
+                request.setColor("blue");
+            }
+            request.setManufacturer("BMW");
 
+            this.webTestClient
+                    .post()
+                    .uri("/api/cars/orders")
+                    .body(Mono.just(request), CarOrder.class)
+                    .exchange()
+                    .expectStatus().is2xxSuccessful() // check status code
+                    .expectBody(Car.class)
+                    .consumeWith(result -> {
+                        assertNotNull(result.getResponseBody().getColor());
+                        assertTrue(result.getResponseBody().getPriceInEuro()
+                                .compareTo(new BigDecimal("1000")) > 0);
+                    });
+        }
     }
 }
